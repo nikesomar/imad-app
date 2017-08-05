0c034202c60331f9507ae13d617d1ce2e5a883ed
nikesomar <30669295+nikesomar@users.noreply.github.com>
2017-08-06 02:14:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,72 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+  title:'ART 1|Nikhil omar',
+  heading:'Article One',
+  pc:'Lenovo flex 2',
+  content:`
+                <p>
+                    the content for first article
+                </p>
+                <p>
+                    This is also a content for first article
+                </p>
+                <p>
+                    another content for first article
+                </p>`
+                    
+    };
+
+function createtemp(data){
+        var title=data.title;
+        var heading=data.heading;
+        var pc=data.pc;
+        var content=data.content;
+        var htmltemp=`
+            <html>
+            <head>
+                <title>
+                  ${title}
+                </title>
+                <meta name="viewport" content="width-device-width, initialscale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+                
+            </head>
+            <body>
+                <div class="BOX">
+                    <div>
+                        <a href='/'>Home</a>
+                    </div>
+                    <hr/>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${pc}
+                    </div>
+                    <div>
+                       ${content}
+                    </div>
+                </div>
+             </body>
+            </html>
+
+    
+        `;    
+
+    return htmltemp;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemp(articleOne));
 });
 
 app.get('/article-two', function (req,res) {
