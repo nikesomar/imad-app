76f74db2d8eee93258b35d2d4ab432c75019149d
nikesomar <30669295+nikesomar@users.noreply.github.com>
2017-08-08 01:13:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,15 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerhtml='LINKIN PARK';
+var img=document.getElementById('lp');
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+1;
+    img.style.marginleft=marginleft+'px';
+    
+}
+img.onclick=function()
+{
+    var interval=setInterval(moveRight,50);
+}
