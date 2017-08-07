57a2b91a366ed8f2e434c3f61c346f87357bf16a
nikesomar <30669295+nikesomar@users.noreply.github.com>
2017-08-08 01:04:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,8 +105,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/lp.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'lp.png'));
 });
 
 
