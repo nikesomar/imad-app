320b459e5c606f9c64d172521011658217af2ce1
nikesomar <30669295+nikesomar@users.noreply.github.com>
2017-08-09 02:26:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,19 @@
 var button= document.getElementById('counter');
-var counter=0;
 button.onClick=function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTMl=counter.toString();
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                 var span=document.getElementById('count');
+                 span.innerHTMl=counter.toString();
+            }
+        }
+    };
+  request.open('GET', 'http://nikesomar.imad.hasura-app.io/counter', true);
+  request.send(null);
+  
+  
+   
 };
 
