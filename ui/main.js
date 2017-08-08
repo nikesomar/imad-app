c0605257fb6ffe762bfac3f18d62634702fe8c4f
nikesomar <30669295+nikesomar@users.noreply.github.com>
2017-08-09 03:33:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,7 @@ button.onClick=function(){
   
    
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
@@ -37,6 +36,8 @@ submit.onclick=function()
             }
         }
     };
+    var nameInput=document.getElementById('name');
+var name=nameInput.value;
   request.open('GET', 'http://nikesomar.imad.hasura-app.io/submit-name?name='+name, true);
   request.send(null);
    
